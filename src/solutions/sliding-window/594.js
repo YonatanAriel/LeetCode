d9df7d7013637bb7afb0bef94f0c8a3cae70e9ac
Yonatan Ariel <yonatanriel@gmail.com>
2024-11-14 23:33:14 +0200

    649 initial approach

--- src/solutions/sliding-window/594.js
+++ src/solutions/sliding-window/594.js
@@ -5,14 +5,18 @@
 export const findLHS = function (nums) {
   let result = 0;
   let counter = 0;
-  let currentNums = {};
+  let currentNums = [nums[0], nums[1]];
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] - nums[i + 1] === 1 || nums[i + 1] - nums[i] === 1) {
+    debugger;
+    if (
+      nums[i] - nums[i + 1] === 1 ||
+      nums[i + 1] - nums[i] === 1 ||
+      nums[i] === nums[i + 1]
+    ) {
       counter++;
-      if (!currentNums.num1) {
-        currentNums.num1 = nums[i];
-        currentNums.num2 = nums[i + 1];
+      if (!currentNums) {
+        currentNums = [nums[i], nums[i + 1]];
       }
     } else {
       result = Math.max(result, counter);
---
+++ src/solutions/sliding-window/649.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+export const findMaxAverage = function (nums, k) {
+  let res = 0;
+  let currentSum = 0;
+
+  for (let i = 0; i <= k; i++) {
+    currentSum += nums[i];
+  }
+  res = currentSum / k;
+
+  for (let i = 1; i < nums.length - k; i++) {
+    currentSum -= nums[i - 1];
+    currentSum += nums[i + k];
+    let avg = currentSum / k;
+    res = Math.max(res, avg);
+  }
+  return res;
+};
