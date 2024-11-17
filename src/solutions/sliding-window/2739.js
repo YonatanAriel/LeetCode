0b7da3ecc0d2b655b459113a8873f8fa44444cb5
Yonatan Ariel <yonatanriel@gmail.com>
2024-11-17 13:29:33 +0200

    improve 2739 - beats 100% & 0ms somehow (again)

--- src/solutions/sliding-window/2739.js
+++ src/solutions/sliding-window/2739.js
@@ -1,27 +1,17 @@
-/**
- * @param {string} blocks
- * @param {number} k
- * @return {number}
- */
-//"WBBWWBBWBW", k =7
-//"WBWBBBW", k = 2
 export const minimumRecolors = function (blocks, k) {
-  const blocksArr = blocks.split("");
-
   let minimumOpr = 0;
   for (let i = 0; i < k; i++) {
-    if (blocksArr[i] === "W") minimumOpr++;
+    if (blocks[i] === "W") minimumOpr++;
   }
 
-  debugger;
-  let operations = minimumOpr;
-  for (let i = 1; i <= blocksArr.length - k; i++) {
+  let currentWhites = minimumOpr;
+  for (let i = 1; i <= blocks.length - k; i++) {
     if (minimumOpr === 0) return 0;
 
-    if (blocksArr[i - 1] === "W") operations--;
-    if (blocksArr[i + k - 1] === "W") operations++;
+    if (blocks[i - 1] === "W") currentWhites--;
+    if (blocks[i + k - 1] === "W") currentWhites++;
 
-    minimumOpr = Math.min(minimumOpr, operations);
+    minimumOpr = Math.min(minimumOpr, currentWhites);
   }
   return minimumOpr;
 };
