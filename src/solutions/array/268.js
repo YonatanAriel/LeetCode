b7c6c97dda4c06033ce4b60463aee20e418719ad
Yonatan Ariel <yonatanriel@gmail.com>
2024-11-22 17:10:44 +0200

    268 optimize solution

--- src/solutions/array/268.js
+++ src/solutions/array/268.js
@@ -5,8 +5,10 @@
 export const missingNumber = function (nums) {
   const sortedNums = nums.sort((a, b) => a - b);
   if (sortedNums[0] !== 0) return 0;
-  for (let i = 0; i < sortedNums.length - 1; i++) {
-    if (sortedNums[i + 1] !== sortedNums[i] + 1) return sortedNums[i] + 1;
+  if (sortedNums[sortedNums.length - 1] !== sortedNums.length)
+    return sortedNums.length;
+
+  for (let i = 1; i < sortedNums.length; i++) {
+    if (sortedNums[i] != i) return i;
   }
-  return sortedNums.length;
 };
