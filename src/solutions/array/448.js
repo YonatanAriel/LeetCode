1dfcc0c6d104ea4bbec77ae05bb11ae4fca69403
Yonatan Ariel <yonatanriel@gmail.com>
2024-12-06 00:15:46 +0200

    448 solution

--- src/solutions/array/448.js
+++ src/solutions/array/448.js
@@ -1,11 +1,24 @@
-export const findDisappearedNumbers = function (nums) {
-  const distinct = [...new Set(nums)].sort((a, b) => a - b);
+// export const findDisappearedNumbers = function (nums) {
+//   const distinct = [...new Set(nums)].sort((a, b) => a - b);
 
-  const allNums = Array.from({ length: nums.length }, (v, k) => k + 1);
+//   const allNums = Array.from({ length: nums.length }, (v, k) => k + 1);
+//   let result = [];
+//   for (let i in allNums) {
+//     if (!distinct.includes(allNums[i])) {
+//       result.push(allNums[i]);
+//     }
+//   }
+//   return result;
+// };
+export const findDisappearedNumbers = function (nums) {
+  const map = new Map();
+  for (let num of nums) {
+    map.set(num, true);
+  }
   let result = [];
-  for (let i in allNums) {
-    if (!distinct.includes(allNums[i])) {
-      result.push(allNums[i]);
+  for (let i = 1; i <= nums.length; i++) {
+    if (!map.get(i)) {
+      result.push(i);
     }
   }
   return result;
