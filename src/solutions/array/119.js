937fa336daf22117094a248bce14a555094dabc2
Yonatan Ariel <yonatanriel@gmail.com>
2024-12-11 23:48:13 +0200

    119 initial solution

---
+++ src/solutions/array/119.js
@@ -0,0 +1,8 @@
+export const getRow = (rowIndex) => {
+  let row = [1];
+  for (let i = 0; i < rowIndex; i++) {
+    row = [1, ...row.map((_, j) => row[j] + (row[j + 1] || 0))];
+  }
+  return row;
+};
+/*!!! */
