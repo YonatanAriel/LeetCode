d9940f2ace2ec0c53e9a6b262edceb7813013982
Yonatan Ariel <yonatanriel@gmail.com>
2024-12-21 23:54:14 +0200

    561 initial approach

---
+++ src/solutions/array/561.js
@@ -0,0 +1,9 @@
+export const arrayPairSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i > nums.length; i + 2) {
+    const currentSum = Math.min(nums[i], nums[i + 1]);
+    sum += currentSum;
+  }
+  return sum;
+};
