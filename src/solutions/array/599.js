49d8cd3213b2c0dd1709e037c8e192ba148e3cc2
Yonatan Ariel <yonatanriel@gmail.com>
2024-12-28 01:01:05 +0200

    599 solution

--- src/solutions/array/599.js
+++ src/solutions/array/599.js
@@ -1,18 +1,30 @@
-export const findRestaurant = function(list1, list2) {
-    const map = new Map();
-    list1.forEach((item, i)) => {
-        map.set(item, {index1: i})
+export const findRestaurant = function (list1, list2) {
+  const map = new Map();
+  list1.forEach((item, i) => {
+    map.set(item, { index1: i });
+  });
+
+  list2.forEach((item, i) => {
+    const value = map.get(item);
+    if (value) {
+      value.index2 = i;
+      map.set(item, value);
     }
-    list2.forEach((item, i)) => {
-        const value = map.get(item) 
-        if(!value) continue
-        value.index2 = i
-        map.set(value)
-    }
-    let result = []
-    for (const [key, value] of map){
-        if (!value.hasOwnProperty('index2')) continue
-        result.push([key, value.index1 + value.index2])
-    }
-    return result.sort((a, b) => a[1] - b[1])]).map(subArray => subArray[0]).pop()
+  });
+
+  let duplicateArr = [];
+  for (const [key, value] of map) {
+    if (!value.hasOwnProperty("index2")) continue;
+    duplicateArr.push([key, value.index1 + value.index2]);
+  }
+
+  const sortedDuplicates = duplicateArr.sort((a, b) => a[1] - b[1]);
+  console.log(sortedDuplicates);
+
+  let result = [sortedDuplicates[0]];
+  for (let i = 1; i < sortedDuplicates.length; i++) {
+    if (sortedDuplicates[i][1] !== result[0][1]) break;
+    result.push(sortedDuplicates[i]);
+  }
+  return result.map((subArray) => subArray[0]);
 };
