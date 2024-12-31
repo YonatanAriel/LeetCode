5c6346933d434251ec255af4a13141ec54d74751
Yonatan Ariel <yonatanriel@gmail.com>
2024-12-31 23:05:08 +0200

    682 solution

--- src/solutions/array/682.js
+++ src/solutions/array/682.js
@@ -1,23 +1,30 @@
-export const calPoints = function(o) {
-    let recordArr = [];
-    let sum = 0;
-    for(let i = 0; i < o.length; i++){
-        if(o[i] === "C") {
-            const prevScore = recordArr.pop();
-            sum -= prevScore;
-        }
-        else if(o[i] === "D"){
-            sum += recordArr[recordArr.length - 2] * 2
-            recordArr.push(recordArr[recordArr.length - 2] * 2)
-        } 
-        else if(o[i] === "+"){
-            sum += sum
-            recordArr.push(sum)
-        } 
-        else {
-            sum += o[i]
-            recordArr.push(o[i])
-        }
+export const calPoints = function (operations) {
+  let recordArr = [];
+  let sum = 0;
+
+  for (let i = 0; i < operations.length; i++) {
+    const op = operations[i];
+
+    if (op === "C") {
+      const prevScore = recordArr.pop();
+      sum -= prevScore;
+    } else if (op === "D") {
+      const lastScore = recordArr[recordArr.length - 1];
+      const newScore = lastScore * 2;
+      recordArr.push(newScore);
+      sum += newScore;
+    } else if (op === "+") {
+      const lastScore = recordArr[recordArr.length - 1];
+      const secondLastScore = recordArr[recordArr.length - 2];
+      const newScore = lastScore + secondLastScore;
+      recordArr.push(newScore);
+      sum += newScore;
+    } else {
+      const newScore = parseInt(op, 10);
+      recordArr.push(newScore);
+      sum += newScore;
     }
-    return sum
+  }
+
+  return sum;
 };
